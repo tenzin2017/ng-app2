a0be1069f7d0bf4592d690455e0b5c82b79b7dee
tenzin2017 <tenzinleo@gmail.com>
2018-10-22 00:11:38 -0400

    components: data binding and displaying on the view template"

---
+++ ng-app2/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, CustomersModule, SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
