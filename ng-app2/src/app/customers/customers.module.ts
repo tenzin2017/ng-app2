5ed6edfb028e6641ca113a08f2fb271f57090c82
tenzin2017 <tenzinleo@gmail.com>
2018-10-22 16:55:18 -0400

    customer filtering feature is added

--- ng-app2/src/app/customers/customers.module.ts
+++ ng-app2/src/app/customers/customers.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
@@ -8,7 +9,7 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CustomersComponent, CustomersListComponent, FilterTextboxComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FormsModule],
   exports: [CustomersComponent]
 })
 export class CustomersModule { }
